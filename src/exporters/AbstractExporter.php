5629d0f8565edd3aa8d91092d37a6759bf4fd65b
Andrii Klochok <andreyklochok@gmail.com>
2026-08-14 18:13:37 +0300

    HQD-309: increase export batch size from 500 to 1000 rows
    Fewer batches means fewer round-trips through the export pipeline for large datasets.

--- src/exporters/AbstractExporter.php
+++ src/exporters/AbstractExporter.php
@@ -31,7 +31,7 @@ abstract class AbstractExporter implements ExporterInterface
 
     public ?GridView $grid = null;
     public bool $exportFooter = true;
-    public int $batchSize = 500;
+    public int $batchSize = 1000;
     public string $target;
     public ExportType $exportType;
     protected ?string $gridClassName = null;
